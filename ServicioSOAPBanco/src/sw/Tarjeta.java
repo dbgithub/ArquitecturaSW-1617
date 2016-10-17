2a06853c68035efb7e6854f726d837459f602561
Aitor DB <aitor.db@opendeusto.es>
2016-10-17 17:50:01 +0200

    pequenos cambios en Tarjeta

--- ServicioSOAPBanco/src/sw/Tarjeta.java
+++ ServicioSOAPBanco/src/sw/Tarjeta.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Tarjeta {
 	
 	public enum EnumProveedores {
-		Visa,Mastercard; 
+		Visa,Mastercard,AmericanExpress; 
 	}
 	public enum TiposTarjeta {
 		Credito,Debito; 
