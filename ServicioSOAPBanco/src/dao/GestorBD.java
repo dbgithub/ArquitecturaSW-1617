013e5e677c7ffa0bfd9c4d56b0bcaa8df0fb1432
daguzman93 <danielagustinas@gmail.com>
2016-10-22 17:23:38 +0200

    Cambios menores GestorBD

--- ServicioSOAPBanco/src/dao/GestorBD.java
+++ ServicioSOAPBanco/src/dao/GestorBD.java
@@ -8,7 +8,7 @@ import sw.Cliente;
 public class GestorBD {
 	private Connection con;
 
-	private String dataSource = "//localhost/ArquitecturaSW";
+	private String dataSource = "//localhost:3306/ArquitecturaSW";
 	private String username = "root";
 	private String password = "root";
 	private String driver = "com.mysql.jdbc.Driver";
@@ -32,24 +32,55 @@ public class GestorBD {
 	public void desconectar() throws SQLException {
 		con.close();
 	}
-	
-	public String obtenerDNI()throws SQLException{
-		
-    	String select = "select dni from CLIENTE";
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(select);
-        String dni= rs.getString("dni");
-        	
-       
-        rs.close();
-        stmt.close();  
-        return dni;
+
+	// public String obtenerDNI() throws SQLException {
+	// String select = "select * from CLIENTE";
+	// Statement stmt = con.createStatement();
+	// ResultSet rs = stmt.executeQuery(select);
+	// String dni = null;
+	// if (rs.next()) {
+	// dni = rs.getString("dni");
+	// }
+	// rs.close();
+	// stmt.close();
+	// return dni;
+	// }
+	public Cliente obtenerClienteDNI(String dni) throws SQLException {
+		Cliente cliente = null;
+		String select = "select * from CLIENTE where dni='" + dni + "'";
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(select);
+		if (rs.next()) {
+			cliente = new Cliente();
+			cliente.setDNI(rs.getString("dni"));
+			cliente.setNombre(rs.getString("nombre"));
+			cliente.setApellidos(rs.getString("apellidos"));
+			cliente.setDireccion(rs.getString("direccion"));
+			cliente.setEmail(rs.getString("email"));
+		}
+		rs.close();
+		stmt.close();
+		return cliente;
 	}
-	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		GestorBD gbd=new GestorBD();
-		gbd.conectar();
-		System.out.println(gbd.obtenerDNI());
-		gbd.desconectar();
+
+	public ArrayList<Cliente> obtenerClientes() throws SQLException {
+		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+		String select = "select * from CLIENTE";
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(select);
+		while (rs.next()) {
+			Cliente cliente = new Cliente();
+			cliente.setDNI(rs.getString("dni"));
+			cliente.setNombre(rs.getString("nombre"));
+			cliente.setApellidos(rs.getString("apellidos"));
+			cliente.setDireccion(rs.getString("direccion"));
+			cliente.setEmail(rs.getString("email"));
+			clientes.add(cliente);
+		}
+		rs.close();
+		stmt.close();
+		return clientes;
 	}
+
+
 }
