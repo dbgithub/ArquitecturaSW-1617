d70335b7c6ef597193b34800c9bd67bc4409a3a7
Aitor DB <aitor.db@opendeusto.es>
2016-10-27 20:51:55 +0200

    Anotaciones JAXB hechas + creacion de un JUnit Test Case vacio incluyendo la libreria  JUnit4

---
+++ ClienteSOAPBanco/src/es/deusto/arquiSW/JAXB/classes/Cuenta.java
@@ -0,0 +1,117 @@
+package es.deusto.arquiSW.JAXB.classes;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlType;
+
+import es.deusto.arquiSW.JAXB.classes.Cliente;
+
+@XmlType(propOrder = { "IBAN", "SWIFT", "fechaApertura", "activa", "saldoActual", "interes", "titular", "tarjeta", "operaciones" }) // opcional
+public class Cuenta {
+	private int IBAN;
+	private String SWIFT;
+	private Date fechaApertura;
+	private boolean activa;
+	private float saldoActual;
+	private float interes;
+	private Cliente titular;
+	private Tarjeta tarjeta;
+	private ArrayList<Operacion> operaciones;
+	
+	
+	/**
+	 * Constructor vacio
+	 */
+	public Cuenta() {
+		
+	}
+	/**
+	 * Constructor
+	 * @param IBAN
+	 * @param SWIFT
+	 * @param fechaApertura
+	 * @param activa
+	 * @param saldoActual
+	 * @param interes
+	 * @param titular
+	 * @param tarjeta
+	 * @param operaciones
+	 */
+	public Cuenta(int iBAN, String sWIFT, Date fechaApertura, boolean activa, float saldoActual, float interes,
+			Cliente titular, Tarjeta tarjeta, ArrayList<Operacion> operaciones) {
+		this.IBAN = iBAN;
+		this.SWIFT = sWIFT;
+		this.fechaApertura = fechaApertura;
+		this.activa = activa;
+		this.saldoActual = saldoActual;
+		this.interes = interes;
+		this.titular = titular;
+		this.tarjeta = tarjeta;
+		this.operaciones = operaciones;
+	}
+	
+	// METHODS & OPERATIONS:
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	public int getIBAN() {
+		return IBAN;
+	}
+	public void setIBAN(int iBAN) {
+		IBAN = iBAN;
+	}
+	public String getSWIFT() {
+		return SWIFT;
+	}
+	public void setSWIFT(String sWIFT) {
+		SWIFT = sWIFT;
+	}
+	public Date getFechaApertura() {
+		return fechaApertura;
+	}
+	public void setFechaApertura(Date fechaApertura) {
+		this.fechaApertura = fechaApertura;
+	}
+	@XmlElement(name = "estaActiva")
+	public boolean isActiva() {
+		return activa;
+	}
+	public void setActiva(boolean activa) {
+		this.activa = activa;
+	}
+	public float getSaldoActual() {
+		return saldoActual;
+	}
+	public void setSaldoActual(float saldoActual) {
+		this.saldoActual = saldoActual;
+	}
+	public float getInteres() {
+		return interes;
+	}
+	public void setInteres(float interes) {
+		this.interes = interes;
+	}
+	@XmlElementWrapper(name = "lista_operaciones")
+	@XmlElement(name = "operacion") // XmlElement especifica el nombre de las entidades
+	public ArrayList<Operacion> getOperaciones() {
+		return operaciones;
+	}
+	public void setOperaciones(ArrayList<Operacion> operaciones) {
+		this.operaciones = operaciones;
+	}
+	public Tarjeta getTarjeta() {
+		return tarjeta;
+	}
+	public void setTarjeta(Tarjeta tarjeta) {
+		this.tarjeta = tarjeta;
+	}
+
+	
+}
