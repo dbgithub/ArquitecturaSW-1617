504cd465cc50bdb7a58f1937eabd84c0c6218756
daguzman93 <danielagustinas@gmail.com>
2016-11-21 23:47:45 +0100

    Implementacion añadir clientes cuentas y tarjetas sin finalizar

--- ClienteRESTBanco/src/es/deusto/arquiSW/REST/gui/FormularioDatosCuentas.java
+++ ClienteRESTBanco/src/es/deusto/arquiSW/REST/gui/FormularioDatosCuentas.java
@@ -8,13 +8,23 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.border.BevelBorder;
+
+import es.deusto.arquiSW.REST.Controller.Controller;
+import es.deusto.arquiSW.REST.DTO.ClienteDTO;
+import es.deusto.arquiSW.REST.DTO.CuentaDTO;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.awt.event.ActionEvent;
 
 public class FormularioDatosCuentas {
@@ -23,6 +33,10 @@ public class FormularioDatosCuentas {
 	private JTextField textFieldFechaApetura;
 	private JTextField textFieldIBAN;
 	private JTextField textFieldDNI;
+	
+	private Controller controlador;
+	private JTextField textFieldSWIFT;
+	private JTextField textFieldSaldo;
 
 	/**
 	 * Launch the application.
@@ -57,7 +71,7 @@ public class FormularioDatosCuentas {
 	private void initialize() {
 		frmAniadirCuenta = new JFrame();
 		frmAniadirCuenta.setTitle("A\u00F1adir cuenta");
-		frmAniadirCuenta.setBounds(100, 100, 559, 224);
+		frmAniadirCuenta.setBounds(100, 100, 559, 266);
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
@@ -85,6 +99,10 @@ public class FormularioDatosCuentas {
 		textFieldDNI.setColumns(10);
 		
 		JComboBox<String> comboBoxInteres = new JComboBox<String>();
+		comboBoxInteres.addItem("0.01");
+		comboBoxInteres.addItem("0.02");
+		comboBoxInteres.addItem("0.06");
+	        
 		
 		JCheckBox checkBoxActiva = new JCheckBox("Activa");
 		checkBoxActiva.setForeground(Color.BLACK);
@@ -93,7 +111,31 @@ public class FormularioDatosCuentas {
 		JButton buttonGuardar = new JButton("Guardar");
 		buttonGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				int dialogbutton = JOptionPane.showConfirmDialog(null,
+						"¿Desea añadir realmente la cuenta especificada ?", "Confirmación", JOptionPane.YES_NO_OPTION);
+				if (dialogbutton == JOptionPane.YES_OPTION) {
+					String IBAN= textFieldIBAN.getText();					
+					String DNICliente = textFieldDNI.getText();
+					String Fecha = textFieldFechaApetura.getText();
+					String Saldo = textFieldSaldo.getText();
+					String SWIFT = textFieldSWIFT.getText();
+					boolean activa = checkBoxActiva.isSelected();
+					Object o = comboBoxInteres.getSelectedItem();
+					String interes= String.valueOf(o);
+					DateFormat format = new SimpleDateFormat();
+					Date date;
+					try {
+						date = format.parse(Fecha);
+						CuentaDTO cuenta = new CuentaDTO(Integer.parseInt(IBAN), SWIFT, date, activa, Float.parseFloat(Saldo), Float.parseFloat(interes), DNICliente, null);
+						controlador.crearCuenta(cuenta);
+						setVisible(false);
+					} catch (ParseException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+
+				}
 			}
 		});
 		
@@ -103,63 +145,91 @@ public class FormularioDatosCuentas {
 				frmAniadirCuenta.setVisible(false);
 			}
 		});
+		
+		JLabel lblSwift = new JLabel("SWIFT");
+		
+		textFieldSWIFT = new JTextField();
+		textFieldSWIFT.setColumns(10);
+		
+		JLabel lblSaldo = new JLabel("Saldo");
+		
+		textFieldSaldo = new JTextField();
+		textFieldSaldo.setColumns(10);
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
-					.addGap(34)
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(60)
 					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(label_1)
+						.addComponent(lblSwift)
+						.addComponent(label_1))
+					.addGap(32)
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(textFieldIBAN, GroupLayout.DEFAULT_SIZE, 381, Short.MAX_VALUE)
+							.addContainerGap())
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(textFieldSWIFT, GroupLayout.DEFAULT_SIZE, 145, Short.MAX_VALUE)
+									.addGap(61))
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(textFieldDNI, GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE)
+									.addGap(22)
+									.addComponent(label)))
+							.addGap(18)
+							.addComponent(textFieldFechaApetura, GroupLayout.DEFAULT_SIZE, 153, Short.MAX_VALUE)
+							.addGap(14))))
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(38)
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblSaldo)
 						.addComponent(label_2)
 						.addComponent(label_3))
-					.addGap(36)
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+					.addGap(32)
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_panel.createSequentialGroup()
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textFieldIBAN, GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE))
+							.addComponent(comboBoxInteres, 0, 109, Short.MAX_VALUE)
+							.addGap(176)
+							.addComponent(checkBoxActiva, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE))
 						.addGroup(gl_panel.createSequentialGroup()
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addComponent(textFieldDNI, GroupLayout.DEFAULT_SIZE, 145, Short.MAX_VALUE)
-								.addGroup(gl_panel.createSequentialGroup()
-									.addComponent(comboBoxInteres, 0, 122, Short.MAX_VALUE)
-									.addGap(23)))
-							.addGap(26)
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_panel.createSequentialGroup()
-									.addComponent(checkBoxActiva, GroupLayout.DEFAULT_SIZE, 81, Short.MAX_VALUE)
-									.addGap(122))
-								.addGroup(gl_panel.createSequentialGroup()
-									.addComponent(label)
-									.addGap(18)
-									.addComponent(textFieldFechaApetura, GroupLayout.DEFAULT_SIZE, 113, Short.MAX_VALUE))
-								.addGroup(gl_panel.createSequentialGroup()
-									.addComponent(buttonGuardar, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addGap(18)
-									.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)))))
-					.addGap(14))
+							.addComponent(textFieldSaldo, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+							.addComponent(buttonGuardar, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
+							.addGap(29)
+							.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
 		);
 		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-					.addContainerGap(13, Short.MAX_VALUE)
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap(25, Short.MAX_VALUE)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(textFieldIBAN, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(label_1))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textFieldDNI, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(textFieldFechaApetura, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblSwift)
+						.addComponent(textFieldSWIFT, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(label_2)
-						.addComponent(label))
+						.addComponent(textFieldFechaApetura, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(label)
+						.addComponent(textFieldDNI, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(comboBoxInteres, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(checkBoxActiva)
-						.addComponent(label_3))
+						.addComponent(label_3)
+						.addComponent(comboBoxInteres, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(buttonGuardar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+							.addComponent(buttonGuardar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+							.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+							.addComponent(lblSaldo)
+							.addComponent(textFieldSaldo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap())
 		);
 		panel.setLayout(gl_panel);
@@ -169,16 +239,15 @@ public class FormularioDatosCuentas {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 519, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(147, Short.MAX_VALUE))
+					.addContainerGap(14, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 159, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(15, Short.MAX_VALUE))
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(166, Short.MAX_VALUE))
 		);
 		frmAniadirCuenta.getContentPane().setLayout(groupLayout);
 	}
-
 }
--- ClienteRESTBanco/src/es/deusto/arquiSW/REST/gui/FormularioDatosTarjetas.java
+++ ClienteRESTBanco/src/es/deusto/arquiSW/REST/gui/FormularioDatosTarjetas.java
@@ -8,12 +8,26 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.border.BevelBorder;
+
+import es.deusto.arquiSW.REST.Controller.Controller;
+import es.deusto.arquiSW.REST.DTO.ClienteDTO;
+import es.deusto.arquiSW.REST.DTO.CuentaDTO;
+import es.deusto.arquiSW.REST.DTO.TarjetaDTO;
+import es.deusto.arquiSW.REST.DTO.OperacionDTO.EnumTipoOperacion;
+import es.deusto.arquiSW.REST.DTO.TarjetaDTO.EnumProveedores;
+import es.deusto.arquiSW.REST.DTO.TarjetaDTO.TiposTarjeta;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.awt.event.ActionEvent;
 
 public class FormularioDatosTarjetas {
@@ -21,6 +35,11 @@ public class FormularioDatosTarjetas {
 	private JFrame frame;
 	private JTextField textFieldNumero;
 	private JTextField textFieldDNI;
+	private JTextField textFieldLimite;
+	private JTextField textFieldFechaCaducidad;
+	private JTextField textFieldFechaExpedicion;
+	private JTextField textFieldCuenta;
+	private Controller controlador;
 
 	/**
 	 * Launch the application.
@@ -54,7 +73,7 @@ public class FormularioDatosTarjetas {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 544, 250);
+		frame.setBounds(100, 100, 544, 296);
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
@@ -85,6 +104,37 @@ public class FormularioDatosTarjetas {
 		JButton btnGuardar = new JButton("Guardar");
 		btnGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				int dialogbutton = JOptionPane.showConfirmDialog(null,
+						"¿Desea añadir realmente la tarjeta especificado ?", "Confirmación", JOptionPane.YES_NO_OPTION);
+				if (dialogbutton == JOptionPane.YES_OPTION) {
+					String numero= textFieldNumero.getText();
+					String limite = textFieldLimite.getText();
+					String fechaca= textFieldFechaCaducidad.getText();
+					String fechaex= textFieldFechaExpedicion.getText();
+					Object oprovee= comboBoxProveedor.getSelectedItem();
+					String prov= String.valueOf(oprovee);
+					Object otipo= comboBoxProveedor.getSelectedItem();
+					String tipo= String.valueOf(otipo);
+					String cuenta= textFieldCuenta.getText();
+					DateFormat format = new SimpleDateFormat();
+					Date dateca;
+					Date dateex;
+					EnumProveedores proveedor=EnumProveedores.valueOf(prov);
+					TiposTarjeta tipot=TiposTarjeta.valueOf(tipo);
+					try {
+						dateca = format.parse(fechaca);
+						dateex= format.parse(fechaex);
+						TarjetaDTO tar= new TarjetaDTO(Integer.parseInt(numero), Integer.parseInt(cuenta), Integer.parseInt(limite), dateca, proveedor, tipot, dateex);
+						controlador.crearTarjeta(tar);
+						setVisible(false);
+					} catch (ParseException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+
+				}
 			}
 		});
 		
@@ -94,36 +144,75 @@ public class FormularioDatosTarjetas {
 				frame.setVisible(false);
 			}
 		});
+		
+		JLabel lblLmiteExtraccion = new JLabel("L\u00EDmite extraccion");
+		
+		textFieldLimite = new JTextField();
+		textFieldLimite.setColumns(10);
+		
+		JLabel lblFechaCaducidad = new JLabel("Fecha caducidad");
+		
+		textFieldFechaCaducidad = new JTextField();
+		textFieldFechaCaducidad.setColumns(10);
+		
+		JLabel lblFechaExpedicin = new JLabel("Fecha expedici\u00F3n");
+		
+		textFieldFechaExpedicion = new JTextField();
+		textFieldFechaExpedicion.setColumns(10);
+		
+		JLabel lblCuente = new JLabel("Cuenta");
+		
+		textFieldCuenta = new JTextField();
+		textFieldCuenta.setColumns(10);
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE))
+							.addComponent(lblLmiteExtraccion)
+							.addContainerGap(405, Short.MAX_VALUE))
 						.addGroup(gl_panel.createSequentialGroup()
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addComponent(label)
-								.addComponent(label_1))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
 							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_panel.createSequentialGroup()
-									.addComponent(comboBoxProveedor, 0, 119, Short.MAX_VALUE)
-									.addGap(46))
+									.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addComponent(label)
+										.addComponent(label_1))
+									.addGap(18)
+									.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+										.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+											.addComponent(textFieldNumero, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
+											.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
+												.addComponent(textFieldLimite, Alignment.TRAILING)
+												.addComponent(textFieldFechaExpedicion, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 113, Short.MAX_VALUE)))
+										.addComponent(comboBoxProveedor, 0, 147, Short.MAX_VALUE)))
 								.addGroup(gl_panel.createSequentialGroup()
-									.addComponent(textFieldNumero, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
-									.addGap(18)))
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addComponent(label_2)
-								.addComponent(label_3))
-							.addGap(18)
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addComponent(comboBoxTipo, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
-								.addComponent(textFieldDNI, GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE))))
-					.addGap(79))
+									.addComponent(lblFechaExpedicin)
+									.addGap(128)))
+							.addGap(10)
+							.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+								.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+									.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addComponent(label_3)
+										.addComponent(label_2))
+									.addGap(30)
+									.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
+										.addComponent(textFieldDNI)
+										.addComponent(comboBoxTipo, 0, 128, Short.MAX_VALUE)))
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE))
+								.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+									.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblFechaCaducidad)
+										.addComponent(lblCuente))
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addComponent(textFieldCuenta, GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE)
+										.addComponent(textFieldFechaCaducidad, GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE))))
+							.addGap(79))))
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
@@ -133,25 +222,35 @@ public class FormularioDatosTarjetas {
 						.addGroup(gl_panel.createSequentialGroup()
 							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 								.addComponent(label_1)
-								.addComponent(textFieldNumero, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addComponent(textFieldNumero, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(label_2))
 							.addGap(18)
-							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(label)
-								.addComponent(comboBoxProveedor, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+							.addComponent(label))
 						.addGroup(gl_panel.createSequentialGroup()
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(textFieldDNI, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(label_2))
+							.addComponent(textFieldDNI, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
 							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 								.addComponent(label_3)
+								.addComponent(comboBoxProveedor, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 								.addComponent(comboBoxTipo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
-					.addPreferredGap(ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblLmiteExtraccion)
+						.addComponent(textFieldLimite, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblFechaCaducidad)
+						.addComponent(textFieldFechaCaducidad, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblFechaExpedicin)
+						.addComponent(textFieldFechaExpedicion, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblCuente)
+						.addComponent(textFieldCuenta, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
-					.addGap(21))
+					.addContainerGap(16, Short.MAX_VALUE))
 		);
 		panel.setLayout(gl_panel);
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
@@ -160,14 +259,14 @@ public class FormularioDatosTarjetas {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 499, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(34, Short.MAX_VALUE))
+					.addContainerGap(19, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
-					.addContainerGap())
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 231, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(80, Short.MAX_VALUE))
 		);
 		frame.getContentPane().setLayout(groupLayout);
 	}
