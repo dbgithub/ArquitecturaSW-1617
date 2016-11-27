53bb99e355b3d1e37918891aa29e59b0c5a81a4f
Aitor DB <aitor.db@opendeusto.es>
2016-11-27 20:22:22 +0100

    Proyecto de Hibernate creado. Archivo de configuracion XML finalizado + clases del dominio hechas + archivos de mapeo definidos

---
+++ PersistenciaBanco/src/es/deusto/arquiSW/classes/Operacion.java
@@ -0,0 +1,78 @@
+package es.deusto.arquiSW.classes;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+public class Operacion {
+	
+	private int id;
+	private Date fecha;
+	private EnumTipoOperacion tipo;
+	private float importe;
+	public enum EnumTipoOperacion {
+		Ingreso,Extraccion; 
+	}
+	private int cuenta;
+	
+	/**
+	 * Constructor vacio
+	 */
+	public Operacion() {
+		
+	}
+	
+	/**
+	 * Constructor
+	 * @param id
+	 * @param fecha
+	 * @param tipo
+	 * @param importe
+	 * @param cuenta
+	 */
+	public Operacion(int id, Date fecha, EnumTipoOperacion tipo, float importe, int cuenta) {
+		this.id = id;
+		this.fecha = fecha;
+		this.tipo = tipo;
+		this.importe = importe;
+		this.cuenta = cuenta;
+	}
+	
+	// METHODS & OPERATIONS:
+		
+		
+	public int getCuenta() {
+		return cuenta;
+	}
+	public void setCuenta(int cuenta) {
+		this.cuenta = cuenta;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public EnumTipoOperacion getTipo() {
+		return tipo;
+	}
+	public void setTipo(EnumTipoOperacion tipo) {
+		this.tipo = tipo;
+	}
+	public float getImporte() {
+		return importe;
+	}
+	public void setImporte(float importe) {
+		this.importe = importe;
+	}
+	
+	
+
+}
---
+++ PersistenciaBanco/src/es/deusto/arquiSW/persistencia/Main.java
@@ -0,0 +1,10 @@
+package es.deusto.arquiSW.persistencia;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Hacer pruebas con Hibernate
+
+	}
+
+}
