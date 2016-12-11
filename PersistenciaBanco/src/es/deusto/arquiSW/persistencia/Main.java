99e2ba960534cca6c8481773a5cba095bc46e534
Aitor DB <aitor.db@opendeusto.es>
2016-12-11 13:21:10 +0100

    Archivo de configuracion de Hibernate y archivos de mapping completados. No se han testado. Falta la parte main del proyecto

--- PersistenciaBanco/src/es/deusto/arquiSW/persistencia/Main.java
+++ PersistenciaBanco/src/es/deusto/arquiSW/persistencia/Main.java
@@ -5,6 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Hacer pruebas con Hibernate
 
+		// A SessionFactory is set up once for an application!
+		// .configure() // configures settings from hibernate.cfg.xml
+		
+		// A Session is used to get a physical connection with a database.
+		
+		// A Transaction represents a unit of work with the database and most of the RDBMS supports transaction functionality.
+		
+		// Dentro de un catch(Exception e) {
+		// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+				// so destroy it manually.
+				//StandardServiceRegistryBuilder.destroy( registry );
+				//}
 	}
 
 }
